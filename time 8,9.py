b093fa2f2ab9dcd2f8e1a9ddccf60702dba43dcf
broxhm-del <broxhm@gmail.com>
2025-09-29 13:03:40 +0200

    forandret

---
+++ time 8,9.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  8 08:19:44 2025
+
+@author: Bruker
+"""
+
+liste2 = [1, 2, 3, 4, 5]
+strengliste = ["alle", "ordene", "i", "lista"]
+for index,element in enumerate(strengliste):
+    print(index, ": ", element)
+    
+  
